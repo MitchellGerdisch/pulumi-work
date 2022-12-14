8ee9dc89ff2574b35b259f91a2585a151cea8140
MitchellGerdisch <mitch@pulumi.com>
2022-12-13 19:22:05 -0600

    Reasonably working stuff

--- pulumi-operator/3_deploy-test-stack/index.ts
+++ pulumi-operator/3_deploy-test-stack/index.ts
@@ -11,20 +11,49 @@ const accessToken = new kx.Secret("accesstoken", {
     stringData: { accessToken: pulumiAccessToken},
 });
 
-// Create a stack in the operator that uses my simple random pet project
-// See: https://github.com/pulumi/pulumi-kubernetes-operator/blob/master/docs/create-stacks-using-pulumi-ts.md#aws-s3-buckets
-const mystack = new k8s.apiextensions.CustomResource("my-stack", {
-  apiVersion: 'pulumi.com/v1',
-  kind: 'Stack',
-  spec: {
-      stack: "MitchGerdisch/simple-stack/k8sopertest",
-      projectRepo: "https://github.com/MitchellGerdisch/pulumi-work",
-      repoDir: "ts-random-simple-stack",
-      branch: "refs/heads/master",
-      accessTokenSecret: accessToken.metadata.name,
-      config: {
-          "simple-stack:petLength": "8",
-      },
-      destroyOnFinalize: true,
-  }
-});
+const numGoodStacks = 2
+for (let i = 0; i < numGoodStacks; i++) {
+
+    // Put in some variability in the config
+    const petLength = (3+(i*2))
+
+    // Create a stack in the operator that uses my simple random pet project
+    // See: https://github.com/pulumi/pulumi-kubernetes-operator/blob/master/docs/create-stacks-using-pulumi-ts.md#aws-s3-buckets
+    const mystack = new k8s.apiextensions.CustomResource(`my-stack-${i}`, {
+    apiVersion: 'pulumi.com/v1',
+    kind: 'Stack',
+    spec: {
+        stack: `MitchGerdisch/simple-stack/k8sopertest-${i}`,
+        projectRepo: "https://github.com/MitchellGerdisch/pulumi-work",
+        repoDir: "ts-random-simple-stack",
+        branch: "refs/heads/master",
+        accessTokenSecret: accessToken.metadata.name,
+        config: {
+            "simple-stack:petLength": `${petLength}`
+        },
+        destroyOnFinalize: true,
+    }
+    });
+}
+
+const numBadStacks = 4
+for (let i = 0; i < numBadStacks; i++) {
+
+    // Create a failed stack in the operator that uses my simple random pet project
+    // See: https://github.com/pulumi/pulumi-kubernetes-operator/blob/master/docs/create-stacks-using-pulumi-ts.md#aws-s3-buckets
+    const mystack = new k8s.apiextensions.CustomResource(`my-bad-stack-${i}`, {
+    apiVersion: 'pulumi.com/v1',
+    kind: 'Stack',
+    spec: {
+        stack: `MitchGerdisch/simple-stack/k8sopertest-fail-${i}`,
+        projectRepo: "https://github.com/MitchellGerdisch/pulumi-work",
+        repoDir: "ts-random-simple-stack",
+        branch: "refs/heads/master",
+        accessTokenSecret: accessToken.metadata.name,
+        config: {
+            "simple-stack:petLength": "badvalue"
+        },
+        destroyOnFinalize: true,
+    }
+    });
+}
--- pulumi-operator/README.md
+++ pulumi-operator/README.md
@@ -12,3 +12,22 @@ Links of note:
   * export KUBECONFIG=kubeconfig.txt
 * Run `pulumi up` for the 3 projects in the numbered order given
   * prometheus needs to be deployed before operator
+  * NOTE the 3_deploy-test-stack stack has a couple of loops with variables to drive how many successful stacks to deploy and how many failing stacks to deploy.
+
+## K8s Operator Helpful Tools
+* `kubectl get stacks`
+* `kubectl get stack STACKNAME_FROM_GET_STACKS -o json`
+
+## Grafana Tips and Tricks
+* Use the JSON found on https://github.com/pulumi/pulumi-kubernetes-operator/blob/master/docs/metrics.md 
+ page to import a new dashboard.
+  * From Dashboards menu icon on the left you'll see an import option.
+  * Select that and import the JSON
+  * Note the Active Stacks will show both successful and failed stacks.
+* To add some semblance of a useful panel for the "stacks_failing" metric do the following:
+  * Click add panel
+  * Select Metric and search for `stacks_failing`
+  * The Select Operations
+    * Select Aggregate - Sum
+  * This results in a graph that accumulates the failed stacks over time.
+
