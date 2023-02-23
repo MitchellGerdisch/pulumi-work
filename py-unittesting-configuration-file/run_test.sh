04f437e08dd76d5ba8ea18646dba2eb686e5b2a8
MitchellGerdisch <mitch@pulumi.com>
2023-02-23 13:21:48 -0600

    implement some code to use secrets in config

--- py-unittesting-configuration-file/run_test.sh
+++ py-unittesting-configuration-file/run_test.sh
@@ -6,7 +6,19 @@ python3 -m venv venv
 source venv/bin/activate
 PROJECT_NAME=${1}
 CONFIG_FILE=${2}
-export PULUMI_CONFIG=$(yq eval ${CONFIG_FILE} --output-format=json | jq -c '.config' | sed "s/${PROJECT_NAME}/project/g")
+
+# This only works for config that has no secrets
+# export PULUMI_CONFIG=$(yq eval ${CONFIG_FILE} --output-format=json | jq -c '.config' | sed "s/${PROJECT_NAME}/project/g")
+# echo $PULUMI_CONFIG | jq "."
+
+## This uses a little python program to process a config with secrets.
+# Infer the stack name from the passed in config file name
+STACK_NAME=$(echo $CONFIG_FILE | sed "s/.*Pulumi\.//g" | cut -d. -f1)
+pulumi_config=$(pulumi config --json --show-secrets --stack $STACK_NAME)
+new_config=`python ./process_config.py -c "$pulumi_config"`
+# Same basic logic as the no secrets example above, but now processing the new_config which supports secrets.
+export PULUMI_CONFIG=$(echo ${new_config} | sed "s/${PROJECT_NAME}/project/g")
+echo $PULUMI_CONFIG | jq "."
 
 echo '\nHit enter to run "python -m unittest"'
 read n
