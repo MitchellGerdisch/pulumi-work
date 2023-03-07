1bd69278e202c8bb41ac42adbe426e03621d0a41
MitchellGerdisch <mitch@pulumi.com>
2023-03-07 09:47:51 -0600

    simplified the policies.

--- ts-policy-validate-resource-relationship/README.md
+++ ts-policy-validate-resource-relationship/README.md
@@ -8,9 +8,7 @@ to be validated via policy.
 
 # Key Items of Note
 * The `validateStack` type of policy is needed to be able to loop through the stack resources to find all the Buckets and then for each bucket to check if there are any BucketPublicAccessBlocks resources that reference it. 
-  * Although `validateStack` does give you access to post-creation properties, it does run during preview and does allow you to see pre-creation properties.
-* However, when creating a `BucketPublicAccessBlock` one can reference the bucket via `bucket` or `id` - they both work. But `id` is not known pre-deployment and 
-  * So this then requires some policies to enforce people to use the `bucket` property but also allow for the post-creation case where `id` is used.
+* Although `validateStack` does give you access to post-creation properties, it does run during preview and does allow you to see pre-creation properties and dependencies.
 
 # How to Use
 * Copy both the `pulumi-project` and `policy-pack` folders.
--- ts-policy-validate-resource-relationship/pulumi-project/index.ts
+++ ts-policy-validate-resource-relationship/pulumi-project/index.ts
@@ -1,10 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-// This first set of resources demonstrate the situation where
-// sunnyDayBucket bucket resource is created and 
-// sunnyDayBucketPublicAccessBlock references it "correctly" using the bucket's "bucket" property instead of the "id" property.
-// In other words, no policies should fire for these two resources.
+// Sunny Day case: Bucket has an access block.
 const sunnyDayBucket = new aws.s3.BucketV2("sunnyDayBucket", {});
 const sunnyDayBucketPublicAccessBlock = new aws.s3.BucketPublicAccessBlock("sunnyDayBucketPublicAccessBlock", {
     bucket: sunnyDayBucket.bucket,
@@ -14,17 +11,6 @@ const sunnyDayBucketPublicAccessBlock = new aws.s3.BucketPublicAccessBlock("sunn
     restrictPublicBuckets: true,
 });
 
-// This set of resources is to validate the case where a BucketPublicAccessBlock resource is created but references
-// the bucket's "id" property instead of the "bucket" property.
-const partlyCloudyBucket = new aws.s3.BucketV2("partlyCloudyBucket", {});
-const partlyCloudyBucketPublicAccessBlock = new aws.s3.BucketPublicAccessBlock("partlyCloudyBucketPublicAccessBlock", {
-  bucket: partlyCloudyBucket.id,
-  blockPublicAcls: true,
-  blockPublicPolicy: true,
-  ignorePublicAcls: true,
-  restrictPublicBuckets: true,
-});
-
 // This set of resources is the main use-case being tested.
-// A bucket is created WITHOUT a BucketPublicAccessBlock being created.
+// A bucket is created WITHOUT a BucketPublicAccessBlock associated with it.
 const rainyDayBucket = new aws.s3.BucketV2("rainyDayBucket", {});
