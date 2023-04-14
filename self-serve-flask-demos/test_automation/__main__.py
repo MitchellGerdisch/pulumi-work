c354788c2e1552aef413df1cb533c1381f72b541
MitchellGerdisch <mitch@pulumi.com>
2023-04-14 13:49:17 -0500

    clean up

--- self-serve-flask-demos/test_automation/__main__.py
+++
@@ -1,7 +0,0 @@
-from pulumi_demos_integration.automate.stack_utils import update_stack
-
-org = "MitchGerdisch"
-project = "test-project"
-stack = "dev"
-stack_outputs = update_stack(org, project, stack, False)
-print(f"main stack outputs: {stack_outputs}")
