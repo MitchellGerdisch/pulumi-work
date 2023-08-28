cf225d7db83de04abcb0469f21290c00f9a30072
Mitch Gerdisch <mitch@pulumi.com>
2023-08-28 17:25:01 -0500

    Update pulumi_search.py

--- hacks/pulumi_search.py
+++ hacks/pulumi_search.py
@@ -2,17 +2,22 @@ import os
 import requests
 
 # Uses Pulumi Search API to search for resources and return the resources all their properties.
+# It uses the `properties` parameter to cause the API to return all the properties for the given resources.
+# It uses the `query` parameter to enable limiting the search.
 # TODO: Leverage the cursor property to manage the case if more than 10,000 resources are returned.
 # - NOTE If the number of resources exceeds 10,000 you will want to modify the query to use cursor to paginate the results.
 #   SEE: https://www.pulumi.com/docs/pulumi-cloud/cloud-rest-api/#parameters-33
+#
+# 
 
 # PREREQUSITES
 # - Set environment variable, PULUMI_ACCESS_TOKEN, to an access token with access to the Pulumi org you are searching in
 # - Set environment variable, PULUMI_ORG, to your pulumi org you are searching in
 
 # HOW TO USE
-# Define a query you want to send if no query is given then ALL resources and ALL their properties will be returned.
+# Define a query below you want to send if no query is given then ALL resources and ALL their properties will be returned.
 # - HELPFUL HINT: Use Pulumi Cloud UI to figure out the query you want to send. The Search AI feature helps with this as well.
+# - If you set `query=""` it will return all resources in the organization. (See note above about >10K resources.)
 
 # EXAMPLE QUERY THAT FINDS RESOURCES FOR A GIVEN PROJECT AND STACK.
 # If you want to find resources for all the stacks for a given project, just remove the "stack:dev" part.
@@ -27,7 +32,6 @@ headers = {
   'Authorization': f"token {pulumi_access_token}"
 }
 
-# r = requests.get(f"https://api.pulumi.com/api/orgs/{pulumi_org}/search/resources?properties=true&query=project:deployment-lambda-eventbridge-dyndb stack:dev", headers = headers)
 r = requests.get(f"https://api.pulumi.com/api/orgs/{pulumi_org}/search/resources?properties=true&query={query}", headers = headers)
 results_json = r.json()
 
