a038f49d7653ccda58155674630019fe22930877
MitchellGerdisch <mitch@pulumi.com>
2023-09-13 11:17:28 -0500

    multi stack support and clean up

--- py-autoapi-deployment-config/main.py
+++ py-autoapi-deployment-config/main.py
@@ -1,54 +1,9 @@
 import sys
 import json
-import pulumi
 from pulumi import automation as auto
-import pulumi_pulumiservice as pulumicloud
 from argsparser import create_parser
-
-# An inline automation API program that configures Deployment settings for a given stack.
-# It is a pretty dumb program with most of the intelligence being set up in the config by the main automation api code.
-def pulumi_program():
-    # Gather up the settings for the deployment configuration
-    config = pulumi.Config()
-    org = config.require("organization")
-    project = config.require("project")
-    stack = config.require("stack")
-
-    # Use the Pulumi Cloud SDK to configure deployments for a given stack
-    deployment = pulumicloud.DeploymentSettings(f"{org}-{project}-{stack}", pulumicloud.DeploymentSettingsArgs(
-        organization=org,
-        project=project,
-        stack=stack,
-        github=pulumicloud.DeploymentSettingsGithubArgs(
-            deploy_commits=True,
-            preview_pull_requests=True,
-            pull_request_template=False,
-            repository=config.require("repository"),
-            paths=[config.require("filter_path")],
-        ),
-        source_context=pulumicloud.DeploymentSettingsSourceContextArgs(
-            git=pulumicloud.DeploymentSettingsGitSourceArgs(
-                branch=config.require("branch"),
-                repo_dir=config.require("repo_dir"),
-            )
-        ),
-        operation_context=pulumicloud.DeploymentSettingsOperationContextArgs(
-            environment_variables={"GITHUB_TOKEN": config.require_secret("github_token")},
-            oidc=pulumicloud.OperationContextOIDCArgs(
-                aws=pulumicloud.AWSOIDCConfigurationArgs(
-                    role_arn=config.require("oidc_role_arn"),
-                    session_name="pulumi-cloud"
-                )
-            ),
-            options=pulumicloud.OperationContextOptionsArgs(
-                skip_install_dependencies=True
-            ),
-            pre_run_commands=["git config --global url.https://${GITHUB_TOKEN}@github.com/.insteadOf https://github.com/"]
-        )
-    ))
-
-    # Export the website URL
-    pulumi.export("Deployment_ID", deployment.id)
+from stack_management import stack_management
+from stacks_base_properties import github_repo, vnext_stacks
 
 # Get the command line arguments
 parser = create_parser()
@@ -59,88 +14,99 @@ destroy = args.destroy
 aws_oidc_arn = args.awsoidc
 github_token =  args.github_token
 
-# Get/set the tenant's project and stack information for which deployments should be set up.
-tenant_project_folder: str
-tenant_project_name:str 
-tenant_stack:str
-tenant_branch_base = "refs/head"
-tenant_branch = f"{tenant_branch_base}/main"
-if args.account:
-    tenant_project_folder = "account"
-    tenant_project_name = f"{tenant}-account"
-    tenant_stack = "account"
-elif args.github:
-   tenant_project_folder = "github"
-   tenant_project_name = f"{tenant}-github"
-   tenant_stack = "github"
-elif args.route53:
-   tenant_project_folder = "route53-record"
-   tenant_project_name = f"{tenant}-route53" # NEED TO CONFIRM THIS NAMING
-   tenant_stack = "records"
-elif args.core:
-   tenant_project_folder = "core-platform"
-   tenant_project_name = f"{tenant}-core"
-   tenant_stack = args.stack
-   if tenant_stack != "prod":
-        tenant_branch = f"{tenant_branch_base}/{tenant_stack}"
-elif args.edge:
-   tenant_project_folder = "edge-platform"
-   tenant_project_name = f"{tenant}-edge"
-   tenant_stack = args.stack
-   if tenant_stack != "prod":
-        tenant_branch = f"{tenant_branch_base}/{tenant_stack}"
-else:
-   print("Must specify at least one project (e.g. -a, -g, -r, -c, -e).\nRun with --help for more information.")
-   sys.exit(255)
+# # Get/set the tenant's project and stack information for which deployments should be set up.
+# tenant_project_folder: str
+# tenant_project_name:str 
+# tenant_stack:str
+# tenant_branch_base = "refs/head"
+# tenant_branch = f"{tenant_branch_base}/main"
+# if args.account:
+#     tenant_project_folder = "account"
+#     tenant_project_name = f"{tenant}-account"
+#     tenant_stack = "account"
+# elif args.github:
+#    tenant_project_folder = "github"
+#    tenant_project_name = f"{tenant}-github"
+#    tenant_stack = "github"
+# elif args.route53:
+#    tenant_project_folder = "route53-record"
+#    tenant_project_name = f"{tenant}-route53" # NEED TO CONFIRM THIS NAMING
+#    tenant_stack = "records"
+# elif args.core:
+#    tenant_project_folder = "core-platform"
+#    tenant_project_name = f"{tenant}-core"
+#    tenant_stack = args.stack
+#    if tenant_stack != "prod":
+#         tenant_branch = f"{tenant_branch_base}/{tenant_stack}"
+# elif args.edge:
+#    tenant_project_folder = "edge-platform"
+#    tenant_project_name = f"{tenant}-edge"
+#    tenant_stack = args.stack
+#    if tenant_stack != "prod":
+#         tenant_branch = f"{tenant_branch_base}/{tenant_stack}"
+# else:
+#    print("Must specify at least one project (e.g. -a, -g, -r, -c, -e).\nRun with --help for more information.")
+#    sys.exit(255)
 
-# Deployment config related settings
-### github_repo = "modusintegration/vnext-tenants"
-github_repo = "pulumi-demos/pulumi-deployments"
 ### repo_dir = f"tenants/{tenant}/{tenant_project_folder}"
-repo_dir = "simple-stack"
-filter_path = f"**/{repo_dir}/**"
+# repo_dir = "simple-stack"
+# filter_path = f"**/{repo_dir}/**"
 
 # project/stack information for the stack that is being run here to create the deployment configuration for the tenant's stack.
 # kinda meta
-project_name = "deployment_configuration"
-stack_name = auto.fully_qualified_stack_name(org, project_name, tenant)
+this_project_name = "stack_management"
+this_stack_name = auto.fully_qualified_stack_name(org, this_project_name, tenant)
 
 # create or select a stack matching the specified name and project.
 # this will set up a workspace with everything necessary to run our inline program (pulumi_program)
-stack = auto.create_or_select_stack(stack_name=stack_name,
-                                    project_name=project_name,
-                                    program=pulumi_program)
+this_stack = auto.create_or_select_stack(stack_name=this_stack_name,
+                                    project_name=this_project_name,
+                                    program=stack_management)
 
 print("successfully initialized stack")
 
 # for inline programs, we must manage plugins ourselves
 print("installing plugins...")
-stack.workspace.install_plugin("pulumiservice", "v0.11.0")
+this_stack.workspace.install_plugin("pulumiservice", "v0.11.0")
 print("plugins installed")
 
-# set stack configuration specifying the AWS region to deploy
+# Test the stacks to see which exist and thus can have Pulumi Cloud settings configured for them
+existing_stacks = []
+for project_info in vnext_stacks:
+    tenant_project_name = f'{tenant}-{project_info["project_basename"]}'
+    tenant_stack_name = auto.fully_qualified_stack_name(org, tenant_project_name, project_info["stack_name"])
+    try:
+        test_stack=auto.select_stack(stack_name=tenant_stack_name, project_name=tenant_project_name, work_dir=".")
+        existing_stacks.append(project_info)
+    except:
+        print(f"Skipping stack (not found): {tenant_stack_name}")
+
+
+
+# set stack configuration to be consumed by the inline program.
 print("setting up config")
-stack.set_config("organization", auto.ConfigValue(value=org))
-stack.set_config("project", auto.ConfigValue(value=tenant_project_name))
-stack.set_config("stack", auto.ConfigValue(value=tenant_stack))
-stack.set_config("repository", auto.ConfigValue(value=github_repo))
-stack.set_config("filter_path", auto.ConfigValue(value=filter_path))
-stack.set_config("branch", auto.ConfigValue(value=tenant_branch))
-stack.set_config("repo_dir", auto.ConfigValue(value=repo_dir))
-stack.set_config("github_token", auto.ConfigValue(value=auto.Secret(github_token)))
-stack.set_config("oidc_role_arn", auto.ConfigValue(value=aws_oidc_arn))
+this_stack.set_config("organization", auto.ConfigValue(value=org))
+this_stack.set_config("tenant", auto.ConfigValue(value=tenant))
+this_stack.set_config("repository", auto.ConfigValue(value=github_repo))
+this_stack.set_config("github_token", auto.ConfigValue(value=auto.Secret(github_token)))
+this_stack.set_config("aws_oidc_role_arn", auto.ConfigValue(value=aws_oidc_arn))
+
+# The existing_stacks information is just pushed out to a file as json instead of trying to create stack config for it.
+with open("existing_stacks.json", "w") as outfile:
+    json.dump(existing_stacks, outfile)
+
 print("config set")
 
 print("refreshing stack...")
-stack.refresh(on_output=print)
+this_stack.refresh(on_output=print)
 print("refresh complete")
 
 if destroy:
     print("destroying stack...")
-    stack.destroy(on_output=print)
+    this_stack.destroy(on_output=print)
     print("stack destroy complete")
     sys.exit()
 
 print("updating stack...")
-up_res = stack.up(on_output=print)
+up_res = this_stack.up(on_output=print)
 print(f"update summary: \n{json.dumps(up_res.summary.resource_changes, indent=4)}")
---
+++ py-autoapi-deployment-config/stacks_base_properties.py
@@ -0,0 +1,43 @@
+# tenant's repo
+github_repo = "modusintegration/vnext-tenants"
+
+# The list of possible vnext stacks
+vnext_stacks = [
+  {
+    "project_basename": "account",
+    "stack_name": "account",
+    "project_folder": "account"
+  },{
+    "project_basename": "github",
+    "stack_name": "github",
+    "project_folder": "github"
+  },{
+    "project_basename": "route53-records",
+    "stack_name": "records",
+    "project_folder": "route53-records"
+  },{
+    "project_basename": "core",
+    "stack_name": "dev",
+    "project_folder": "core-platform"
+  },{
+    "project_basename": "core",
+    "stack_name": "stg",
+    "project_folder": "core-platform"
+  },{
+    "project_basename": "core",
+    "stack_name": "prod",
+    "project_folder": "core-platform"
+  },{
+    "project_basename": "edge",
+    "stack_name": "dev",
+    "project_folder": "edge-platform"
+  },{
+    "project_basename": "edge",
+    "stack_name": "stg",
+    "project_folder": "edge-platform"
+  },{
+    "project_basename": "edge",
+    "stack_name": "prod",
+    "project_folder": "edge-platform"
+  }
+]
