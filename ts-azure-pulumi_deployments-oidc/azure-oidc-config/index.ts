c048eadd12e0506632fb74fa364318ba1c2dc41a
MitchellGerdisch <mitch@pulumi.com>
2023-10-09 12:30:21 -0500

    resource group role and related updates

--- ts-azure-pulumi_deployments-oidc/azure-oidc-config/index.ts
+++ ts-azure-pulumi_deployments-oidc/azure-oidc-config/index.ts
@@ -21,8 +21,10 @@ const name = "demo-deploy-oidc"
 const currentClient = auth.getClientConfigOutput()
 const clientOwners = [currentClient.objectId]
 export const subscriptionId = currentClient.subscriptionId
+const fullSubId = pulumi.interpolate`subscriptions/${subscriptionId}`
 export const tenantId = currentClient.tenantId
 
+// Create Azure application
 const appName = `${name}-app`
 const adApp = new azuread.Application(appName, {
     displayName: appName,
@@ -30,6 +32,7 @@ const adApp = new azuread.Application(appName, {
 });
 export const clientId = adApp.applicationId
 
+// Configure necessary subjects needed for Pulumi deployments Azure OIDC support.
 // See: https://www.pulumi.com/docs/pulumi-cloud/deployments/oidc/azure/#adding-federated-credentials
 const fedIdCredName = `${name}-fed-id-cred`
 const stackActions = ["preview", "update", "refresh", "destroy"]
@@ -46,33 +49,58 @@ for (let stackAction of stackActions) {
     })
 }
 
+// Create a service principal for the app.
 const adSp = new azuread.ServicePrincipal(`${name}-sp`, {
     applicationId: adApp.applicationId,
 });
 
+// Now assigne roles to 
 // Using Azure built-in roles which have UUIDs for their "names"
 // This page provides links to the various built-in roles and by clicking through their names you'll see JSON which provides the role ID
 // https://learn.microsoft.com/en-us/azure/role-based-access-control/built-in-roles
 
 // Identifying a few of these roles that are applicable to the project being managed by this OIDC thing
 // A king and pope role
-const CONTRIBUTOR="b24988ac-6180-42a0-ab88-20f7382dd24c"
+// const CONTRIBUTOR="b24988ac-6180-42a0-ab88-20f7382dd24c"
 // More targeted to the ts-azure-storage project that is colocated with this code
 const STORAGE_ACCOUNT_CONTRIBUTOR="17d1049b-9a84-46fb-8f53-869881c3d3ab"
 const STORAGE_BLOB_DATA_CONTRIBUTOR="ba92f5b4-2d11-453d-a403-e96b0029c9fe"
+const STORAGE_ACCOUNT_KEY_OPERATOR="81a9662b-bebf-436f-a333-f67b29880f12"
 
-// Obviously, creating role assignments for the storage contributor roles is unnecessary given the CONTRIBUTOR role.
-// But wanted to show/test looping through a set of different roles.
-// const roleIds = [CONTRIBUTOR, STORAGE_ACCOUNT_CONTRIBUTOR, STORAGE_BLOB_DATA_CONTRIBUTOR]
-// Testing what should be "minimal" permissions needed.
-const roleIds = [STORAGE_ACCOUNT_CONTRIBUTOR, STORAGE_BLOB_DATA_CONTRIBUTOR]
-const fullSubId = pulumi.interpolate`subscriptions/${subscriptionId}`
-for (let roleId of roleIds) {
-    const roleDefinitionId = pulumi.interpolate`${fullSubId}/providers/Microsoft.Authorization/roleDefinitions/${roleId}`
-    const roleAssignment = new auth.RoleAssignment(roleId, {
+// There is no built-in role for resource group management.
+// So creating one ...
+// Define the role
+const rgRoleName = `${name}-rg-role`
+const rgRoleDef = new auth.RoleDefinition(rgRoleName, {
+    scope: fullSubId,
+    assignableScopes: [fullSubId],
+    roleName: rgRoleName,
+    permissions: [{
+        actions: [ // Here you define the permissions this role has
+            "Microsoft.Resources/subscriptions/resourceGroups/write",
+            "Microsoft.Resources/subscriptions/resourceGroups/delete",
+        ],
+        notActions: [],
+    }],
+    description: "This is a custom role that manages resource groups.",
+});
+
+// Could just set the role assignment to CONTRIBUTOR role and be done with things.
+// But this way shows creating assignments for a set of more limited roles.
+const rolesInfo = [
+    {name: rgRoleName, roleDefId: rgRoleDef.id}, 
+    {name: "STORAGE_ACCOUNT_CONTRIBUTOR", roleDefId: pulumi.interpolate`${fullSubId}/providers/Microsoft.Authorization/roleDefinitions/${STORAGE_ACCOUNT_CONTRIBUTOR}`}, 
+    {name: "STORAGE_BLOB_DATA_CONTRIBUTOR", roleDefId: pulumi.interpolate`${fullSubId}/providers/Microsoft.Authorization/roleDefinitions/${STORAGE_BLOB_DATA_CONTRIBUTOR}`},
+    {name: "STORAGE_ACCOUNT_KEY_OPERATOR", roleDefId: pulumi.interpolate`${fullSubId}/providers/Microsoft.Authorization/roleDefinitions/${STORAGE_ACCOUNT_KEY_OPERATOR}`},
+]
+
+for (let roleInfo of rolesInfo) {
+    const roleName = roleInfo.name
+    const roleDefId = roleInfo.roleDefId
+    const roleAssignment = new auth.RoleAssignment(roleName, {
         principalId: adSp.id, 
         principalType: "ServicePrincipal",
-        roleDefinitionId: roleDefinitionId,
+        roleDefinitionId: roleDefId,
         scope: fullSubId,
     });
 }
@@ -118,14 +146,8 @@ echo "arm_oidc_request_token: $ARM_OIDC_REQUEST_TOKEN"
 echo "arm_oidc_request_url: $ARM_OIDC_REQUEST_URL"
 az login --service-principal -u $ARM_CLIENT_ID -t $ARM_TENANT_ID --federated-token $ARM_OIDC_TOKEN
 
-## This one was used to validate the OIDC stuff actually works for 
+## This one was used to validate the OIDC stuff actually works 
 ARM_OIDC_REQUEST_URL=https://api.pulumi.com/oidc ARM_OIDC_REQUEST_TOKEN=$ARM_OIDC_TOKEN /pulumi-deploy-executor pulumi preview --stackIdentity="MitchGerdisch/ts-azure-storage/dev" --workDir="/deployment/ts-azure-pulumi_deployments-oidc/ts-azure-storage"
 
 */
 
-/* 
-# Another prerun command to get a sense of what might be going on
-# NOTE: You'll need to watch deployments and do the azure web device login stuff with the given code.
-# Using the az login with the federated-token as given above doesn't work since the provider does not support "az login" with service principal
-ARM_USE_OIDC=false; az login; /pulumi-deploy-executor pulumi preview --stackIdentity="MitchGerdisch/ts-azure-storage/dev" --workDir="/deployment/ts-azure-pulumi_deployments-oidc/ts-azure-storage"
-*/
