079d51051ee3b1284c46a02a84840a1e30d89f2b
MitchellGerdisch <mitch@pulumi.com>
2024-01-30 18:43:58 -0600

    working version of py-blue-green

--- py-blue-green-apigw/aws-py-apigateway-lambda-serverless/__main__.py
+++ py-blue-green-apigw/aws-py-apigateway-lambda-serverless/__main__.py
@@ -13,14 +13,12 @@ config = pulumi.Config()
 hosted_zone = config.get("hosted_zone") or "pulumi-ce.team"
 hosted_zone_id = aws.route53.get_zone(name=hosted_zone).id
 custom_stage_name = config.get("stage_name") or "my-app"
-domain_name = f"{stack_name}.{hosted_zone}"
 
 ##################
 ## Lambda Function
 ##################
 
-# Create a Lambda function, using code from the `./app` folder.
-
+# Create a Lambda function, using code from the hello_lambda_X folder.
 lambda_func = aws.lambda_.Function("mylambda",
     role=iam.lambda_role.arn,
     runtime="python3.7",
@@ -30,55 +28,7 @@ lambda_func = aws.lambda_.Function("mylambda",
     })
 )
 
-# Request ACM certificate
-ssl_cert = aws.acm.Certificate("ssl-cert",
-                                domain_name=domain_name,
-                                validation_method="DNS",
-)
-
-# Create DNS record to prove to ACM that we own the domain
-ssl_cert_validation_dns_record = aws.route53.Record("ssl-cert-validation-dns-record",
-                                                    zone_id=hosted_zone_id,
-                                                    name=ssl_cert.domain_validation_options.apply(
-                                                        lambda options: options[0].resource_record_name),
-                                                    type=ssl_cert.domain_validation_options.apply(
-                                                        lambda options: options[0].resource_record_type),
-                                                    records=[ssl_cert.domain_validation_options.apply(
-                                                        lambda options: options[0].resource_record_value)],
-                                                    ttl=10*60)
-# Wait for the certificate validation to succeed
-validated_ssl_certificate = aws.acm.CertificateValidation("ssl-cert-validation",
-                                                            certificate_arn=ssl_cert.arn,
-                                                            validation_record_fqdns=[ssl_cert_validation_dns_record.fqdn],
-)
-# Configure API Gateway to be able to use domain name & certificate
-api_domain_name = aws.apigatewayv2.DomainName("api-domain-name",
-    domain_name=domain_name,
-    domain_name_configuration={
-        "certificate_arn": ssl_cert.arn,
-        "endpoint_type": "REGIONAL",
-        "security_policy": "TLS_1_2"
-    },
-    opts=pulumi.ResourceOptions(depends_on=[validated_ssl_certificate]))
-
-
-# Create DNS record for this blue or green stack
-dns = aws.route53.Record("api-dns",
-    name=domain_name,
-    type="A",
-    zone_id=hosted_zone_id,
-    aliases=[aws.route53.RecordAliasArgs(
-        name=api_domain_name.domain_name_configuration.target_domain_name,
-        zone_id=api_domain_name.domain_name_configuration.hosted_zone_id,
-        evaluate_target_health=False,
-    )])
-
-#########################################################################
 # Create an HTTP API and attach the lambda function to it
-##    /{proxy+} - passes all requests through to the lambda function
-##
-#########################################################################
-
 http_endpoint = aws.apigatewayv2.Api("http-api-pulumi-example",
     protocol_type="HTTP",
 )
@@ -92,8 +42,6 @@ http_lambda_backend = aws.apigatewayv2.Integration("example",
     integration_uri=lambda_func.arn,
     passthrough_behavior="WHEN_NO_MATCH"
 )
-
-# url = http_lambda_backend.integration_uri
 
 http_route = aws.apigatewayv2.Route("example-route",
     api_id=http_endpoint.id,
@@ -114,12 +62,6 @@ http_stage = aws.apigatewayv2.Stage("example-stage",
     auto_deploy=True
 )
 
-api_mapping = aws.apigatewayv2.ApiMapping("api_mapping", aws.apigatewayv2.ApiMappingArgs (
-  domain_name=domain_name,
-  api_id=http_endpoint.id,
-  stage=http_stage.name
-), opts=pulumi.ResourceOptions(depends_on=[api_domain_name]))
-
 # Give permissions from API Gateway to invoke the Lambda
 http_invoke_permission = aws.lambda_.Permission("api-http-lambda-permission",
     action="lambda:invokeFunction",
@@ -136,6 +78,6 @@ stack_tag = pulumiservice.StackTag("stacktag", pulumiservice.StackTagArgs(
   value="blue-green"
 ))
 
-pulumi.export("api_endpoint", dns.fqdn)
+pulumi.export("api_id", http_endpoint.id)
 pulumi.export("stage_name", http_stage.name)
 pulumi.export(f"{stack_name} api gateway url", pulumi.Output.concat(http_endpoint.api_endpoint, "/", http_stage.name, "/"))
--- py-blue-green-apigw/blue-green-dns/__main__.py
+++ py-blue-green-apigw/blue-green-dns/__main__.py
@@ -14,8 +14,9 @@ app_project_name = config.get("app_project_name") or "aws-py-apigateway-lambda-s
 
 active_system_name = config.require("active_system_name")
 
+# Get values from the stack that is now considered the active environment.
 stackref = pulumi.StackReference(f"{pulumi.get_organization()}/{app_project_name}/{active_system_name}")
-api_endpoint = stackref.get_output("api_endpoint")
+api_id = stackref.get_output("api_id")
 stage_name = stackref.get_output("stage_name")
 
 domain_name = f"hello.{hosted_zone}"
@@ -42,34 +43,34 @@ validated_ssl_certificate = aws.acm.CertificateValidation("ssl-cert-validation",
                                                             validation_record_fqdns=[ssl_cert_validation_dns_record.fqdn],
 )
 
-# # Create DNS record for this blue or green stack
-# dns = aws.route53.Record("api-dns",
-#     name=domain_name,
-#     type="A",
-#     zone_id=hosted_zone_id,
-#     aliases=[aws.route53.RecordAliasArgs(
-#         name=api_domain_name.domain_name_configuration.target_domain_name,
-#         zone_id=api_domain_name.domain_name_configuration.hosted_zone_id,
-#         evaluate_target_health=False,
-#     )])
+# Configure "active" API Gateway to be able to use domain name & certificate
+api_domain_name = aws.apigatewayv2.DomainName("api-domain-name",
+    domain_name=domain_name,
+    domain_name_configuration={
+        "certificate_arn": ssl_cert.arn,
+        "endpoint_type": "REGIONAL",
+        "security_policy": "TLS_1_2"
+    },
+    opts=pulumi.ResourceOptions(depends_on=[validated_ssl_certificate]))
 
-# Create dns record.
-# active_system_record = aws.route53.Record('active_system',
-#     zone_id=hosted_zone_id,
-#     name=domain_name,
-#     type='CNAME',
-#     records=[api_endpoint],
-#     ttl=30)
-
+# Create active DNS record
 active_system_record = aws.route53.Record('active_system',
     zone_id=hosted_zone_id,
     name=domain_name,
     type='A',
     aliases=[aws.route53.RecordAliasArgs(
-      name=api_endpoint,
-      zone_id=hosted_zone_id,
+      name=api_domain_name.domain_name_configuration.target_domain_name,
+      zone_id=api_domain_name.domain_name_configuration.hosted_zone_id,
       evaluate_target_health=False,
     )])
+
+# Create mapping of domain name to active api gw
+api_mapping = aws.apigatewayv2.ApiMapping("api_mapping", aws.apigatewayv2.ApiMappingArgs (
+  domain_name=domain_name,
+  api_id=api_id,
+  stage=stage_name,
+), opts=pulumi.ResourceOptions(depends_on=[api_domain_name], delete_before_replace=True))
+
 
 pulumi.export("active url", pulumi.Output.concat("https://", active_system_record.fqdn,"/",stage_name))
 
