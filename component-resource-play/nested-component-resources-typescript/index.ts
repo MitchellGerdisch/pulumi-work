8c2ef4447573a567f2513fbceaceb43d411e264d
MitchellGerdisch <mitch@pulumi.com>
2024-03-21 11:00:24 -0500

    some updates

--- component-resource-play/nested-component-resources-typescript/index.ts
+++ component-resource-play/nested-component-resources-typescript/index.ts
@@ -6,4 +6,3 @@ const bucket = new s3bucket.S3Bucket("my-bucket", {});
 
 // Export the name of the bucket
 export const bucketName = bucket.bucketId
-export const bucket2Name = bucket.bucket2.bucketId
--- component-resource-play/nested-component-resources-typescript/s3bucket.ts
+++ component-resource-play/nested-component-resources-typescript/s3bucket.ts
@@ -1,7 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import { Input, Output } from "@pulumi/pulumi"
 import * as aws from "@pulumi/aws"
-import * as s3bucket2 from "./s3bucket_2"
 
 interface S3BucketArgs {
   acl?: Input<string>;
@@ -10,16 +9,13 @@ interface S3BucketArgs {
 export class S3Bucket extends pulumi.ComponentResource {
     public readonly bucketId: Output<string>;
     public readonly bucket: aws.s3.Bucket;
-    public readonly bucket2: s3bucket2.S3Bucket2;
 
     constructor(name: string, args: S3BucketArgs, opts?: pulumi.ComponentResourceOptions) {
-        super("custom:x:S3Bucket", name, args, opts);
-        this.bucket = new aws.s3.Bucket(name, {
+      super("custom:x:S3Bucket", name, args, opts);
+        this.bucket = new aws.s3.Bucket(`${name}-comp`, {
           acl: args.acl
         }, {parent: this})
 
         this.bucketId = this.bucket.id
-
-        this.bucket2 = new s3bucket2.S3Bucket2(name, {}, {parent: this})
     }
   }
